7228c91a13c5623fe9bbc2f551f5c016519b1aea
BasicMicro <info@basicmicro.com>
2018-11-28 15:16:47 -0800

    Added Arduino sketch

--- standard_serial_bus.ino
+++ standard_serial_bus.ino
@@ -1,9 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+// Pins to be used for slave select function
+#define controller1 2
+#define controller2 3
+#define controller3 4
+#define controller4 5
+
+SoftwareSerial serial(10,11);
+
+void setup() {
+  // Set the slave select pins as outputs
+  pinMode(controller1,OUTPUT);
+  pinMode(controller2,OUTPUT);
+  pinMode(controller3,OUTPUT);
+  pinMode(controller4,OUTPUT);
+
+  serial.begin(38400);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // We will select each controller in sequentially and operate the motor attached to each
+  digitalWrite(controller1, HIGH);
+  serial.write(94);
+  delay(2000);
+  serial.write((byte)0);
+  digitalWrite(controller1, LOW);
+  delay(2000);
+  
 
+  digitalWrite(controller2, HIGH);
+  serial.write(94);
+  delay(2000);
+  serial.write((byte)0);
+  digitalWrite(controller2, LOW);
+  delay(2000);
+ 
+
+  digitalWrite(controller3, HIGH);
+  serial.write(94);
+  delay(2000);
+  serial.write((byte)0);
+  digitalWrite(controller3, LOW);
+  delay(2000);
+  
+
+  digitalWrite(controller4, HIGH);
+  serial.write(94);
+  delay(2000);
+  serial.write((byte)0);
+  digitalWrite(controller4, LOW);
+  delay(2000);
+  
 }
