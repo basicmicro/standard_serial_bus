6ff2bcd23bf646eebde185035fe6e1f9bbbdf44e
BasicMicro <info@basicmicro.com>
2018-11-28 16:50:41 -0800

    Added run_controller function

--- standard_serial_bus.ino
+++ standard_serial_bus.ino
@@ -6,6 +6,8 @@
 #define controller3 4
 #define controller4 5
 
+void run_controller(int, int, int);
+
 SoftwareSerial serial(10,11);
 
 void setup() {
@@ -20,42 +22,27 @@ void setup() {
 
 void loop() {
   // We will select each controller sequentially and operate the motor attached to each
-  digitalWrite(controller1, HIGH);
-  delay(50);
-  serial.write(94);
+  run_controller(controller1, 94, 2000);
   delay(2000);
-  serial.write((byte)0);
-  delay(50);
-  digitalWrite(controller1, LOW);
-  delay(2000);
-  
 
-  digitalWrite(controller2, HIGH);
-  delay(50);
-  serial.write(94);
+  run_controller(controller2, 94, 2000);
   delay(2000);
-  serial.write((byte)0);
-  delay(50);
-  digitalWrite(controller2, LOW);
-  delay(2000);
- 
 
-  digitalWrite(controller3, HIGH);
-  delay(50);
-  serial.write(94);
+  run_controller(controller3, 94, 2000);
   delay(2000);
-  serial.write((byte)0);
-  delay(50);
-  digitalWrite(controller3, LOW);
-  delay(2000);
-  
 
-  digitalWrite(controller4, HIGH);
-  delay(50);
-  serial.write(94);
+  run_controller(controller4, 94, 2000);
   delay(2000);
+}
+
+
+void run_controller(int controller, int speed, int run_time){
+  // run_time is in milliseconds
+  digitalWrite(controller, HIGH);
+  delay(50);
+  serial.write(speed);
+  delay(run_time);
   serial.write((byte)0);
   delay(50);
-  digitalWrite(controller4, LOW);
-delay(2000);
+  digitalWrite(controller, LOW);  
 }
